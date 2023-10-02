c8c45f1c560fb37d34e6de29a1136f7282fd1417
ArifDoloksaribu <143679155+ArifDoloksaribu@users.noreply.github.com>
2023-10-01 23:24:09 -0700

    Arif M. Doloksaribu

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,29 @@
-// NIM - Your Name
+// 12S23026-Arif M. Doloksaribuu
+import java.util.*;
+import java.lang.Math;
+
+class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String keterangan;
+        int y;
+
+        System.out.println("masukkan angka");
+        y = input.nextInt();
+        if (y > 3 && y <= 5) {
+            keterangan = "Penting! Harus segera dikerjakan";
+        } else {
+            if (y <= 3 && y >= 1.5) {
+                keterangan = " Tugas ini memiliki prioritas menengah";
+            } else {
+                if (y < 1.5) {
+                    keterangan = "Tugas ini relatif ringan, namun jangan tunda terlalu lama";
+                } else {
+                    keterangan = "Tugas ini relatif ringan, namun jangan tunda terlalu lama";
+                }
+            }
+        }
+        System.out.println(keterangan);
+    }
+}
